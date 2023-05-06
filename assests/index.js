96fee69184a58eb580ee34cb18fc143f1ad67f2b
jooaoVSilva <joao.silva850@sptech.school>
2023-05-06 17:55:54 -0300

    form-cadastro

--- assests/index.js
+++ assests/index.js
@@ -1,17 +1,7 @@
-var myModal_cadastro = document.getElementById("myModal_cadastro");
-var cadastro_button = document.getElementById("cadastro");
-var span = document.getElementsByClassName("close")[0];
+var modal = document.getElementById('cadastro');
 
-cadastro_button.onclick = function(){
-    myModal_cadastro.style.display = "block";
-}
-
-span.onclick = function(){
-    myModal_cadastro.style.display = "none";
-}
-
-window.onclick = function(event){
-    if (event.target == myModal_cadastro){
-        myModal_cadastro.style.display = "none";
-    }
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
